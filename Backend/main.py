98fdc55195d3782f35717a60d86d8ae2e1ec90a7
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2020-08-24 22:12:15 +0200

    Add middleware and missing requirements for alembic

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from blockchain.blockchain import Blockchain
+from fastapi_sqlalchemy import DBSessionMiddleware
 
 # Start applications
 app = FastAPI()
 blockchain_app = Blockchain()
+
+# Middleware
+app.add_middleware(
+    DBSessionMiddleware,
+    db_url='postgresql+psycopg2://postgres:postgres@postgres:5432',
+)
 
 # Import API views
 from urls import *
